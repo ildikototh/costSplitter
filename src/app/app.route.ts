d22a5ee6da7b90da3a3d6e4661401104ad4b6d44
Peter De Winter <peter.de.winter@hailforge.com>
2017-06-16 15:34:40 +0100

    Merge remote-tracking branch 'remotes/origin/feature/authentication' into develop
    # Conflicts:
    #	src/app/app.route.ts

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -17,8 +17,7 @@ export const router: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login-email', component: EmailComponent },
-  { path: 'members', component: MemberComponent, canActivate: [AuthService] }
-  { path: 'members', component: MemberComponent},
+  { path: 'members', component: MemberComponent, canActivate: [AuthService] },
   { path: 'home', component: HomeComponent},
   { path: 'expenses', loadChildren: './expenses/expenses.module.ts#ExpensesModule' },
   { path: '**', component: NotfoundComponent}
