9cd25ef673de266085091efdd3521691b0c91bfc
pvanrolleghem <pieter.van.rolleghem@m-team.be>
2017-06-16 15:19:51 +0200

    Merge remote-tracking branch 'origin/develop' into group-module
    # Conflicts:
    #	package.json
    #	src/app/app.component.html
    #	src/app/app.module.ts

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Npm installs
 
-Run npm install ngx-qrcode2 --save` to install the QR code module.
+Run npm `install ngx-qrcode2 --save` to install the QR code module.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http'
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AngularFireModule} from "angularfire2";
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireAuthModule} from "angularfire2/auth";
 
-import {firebaseConfig} from '../environments/environment';
-import { LoginComponent } from './login/login.component';
-import { EmailComponent } from './email/email.component';
-import { SignupComponent } from './signup/signup.component';
-import { MemberComponent } from './member/member.component';
-import { AuthService } from './shared/services/auth.service';
-import { routes } from './app.route';
+import {firebaseConfig} from "../environments/environment";
+import {LoginComponent} from "./login/login.component";
+import {EmailComponent} from "./email/email.component";
+import {SignupComponent} from "./signup/signup.component";
+import {MemberComponent} from "./member/member.component";
+import {routes} from "./app.route";
 import {AppComponent} from "./app.component";
 import {GroupService} from "./shared/services/group.service";
 import {GroupModule} from "./group/group.module";
