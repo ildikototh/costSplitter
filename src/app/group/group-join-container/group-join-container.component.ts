d0a3b8aab056d9b12a00a301d83cf282409b04b4
Peter De Winter <peter.de.winter@hailforge.com>
2017-06-17 13:44:09 +0100

    Added some logic for the user to show.

--- src/app/group/group-join-container/group-join-container.component.ts
+++ src/app/group/group-join-container/group-join-container.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {GroupService} from "../../shared/services/group.service";
 import {AngularFireAuth} from "angularfire2/auth";
 import {User} from "firebase/app";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-group-join-container',
@@ -12,7 +13,7 @@ export class GroupJoinContainerComponent implements OnInit {
 
   currentUser:User;
 
-  constructor(private groupService:GroupService, public af: AngularFireAuth) {
+  constructor(private groupService:GroupService, public af: AngularFireAuth, private router: Router) {
     this.af.authState.subscribe(auth => {
       console.log(auth)
       if (auth) {
@@ -26,6 +27,7 @@ export class GroupJoinContainerComponent implements OnInit {
 
   addUserToGroup(event: any) {
     this.groupService.addUserToGroup(this.currentUser.uid, event.groupId);
+    this.router.navigate(['/members'])
   }
 
 }
